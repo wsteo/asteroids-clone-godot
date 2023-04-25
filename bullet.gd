7cba5a26fd9fa58897f6b4f725d444314500d931
wsteo <35697328+wsteo@users.noreply.github.com>
2023-04-25 18:43:15 +0800

    Format code

--- bullet.gd
+++ bullet.gd
@@ -5,4 +5,4 @@ extends RigidBody2D
 var velocity = Vector2(0,0)
 
 func _physics_process(delta):
-	move_and_collide(velocity.normalized()*delta*speed)
+	move_and_collide(velocity.normalized() * delta * speed)
